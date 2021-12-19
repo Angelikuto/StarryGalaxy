0897cbd0c09d8077d63a4684c61fc46af70cde2b
Angelikuto <92451895+Angelikuto@users.noreply.github.com>
2021-12-19 19:17:36 +0100

    uptade

---
+++ src/Tableau1.js
@@ -0,0 +1,103 @@
+/**
+ * ALGO: ceci est une classe...
+ * Vous verrez ça plus tard en détail avec Rémi, pour l'instant on n'a pas trop besoin de savoir à quoi ça sert.
+ */
+class Tableau1 extends Phaser.Scene{
+    /**
+     * Précharge les assets
+     */
+    preload(){
+        for(let i=1;i<=10;i++){
+            this.load.image('idle'+ i, 'Characters/boy/boy_style_5/PNG/idle2/Layer-' + i + '.png');
+
+        this.load.image('bg2-terrain-2', 'assets/level/background-2/bg2-terrain-2.png');
+        //au lieu d'écrire 5 lignes quasi identiques, on charge l'herbe avec une boucle
+        // ALGO : ceci est une boucle
+        for(let i=1;i<=5;i++){
+            this.load.image('g-grass-'+i, 'assets/level/ground/g-grass-'+i+'.png');
+        }
+
+
+        //filtre film TODO élève : faire une boucle à la place des 3 lignes qui suivent
+        for(let i=1;i<=3;i++) {
+            this.load.image('filterFilm' + i, 'assets/level/filters/bloody/frame' + i + '.png');
+        }
+
+
+        //texture au fond  TODO élève : faire une boucle pour charger les 3 images et démontrer par la même que vous savez aller au plus simple
+        this.load.image('bg-animation-a', 'assets/level/background-2/bg-animation/bg-animation-a.png');
+
+    }
+    getFrames(prefix,length){
+        let frames=[];
+        for (let i=1;i<=length;i++){
+            frames.push({key: prefix+i});
+        }
+        return frames;
+    }
+    /**
+     * Crée la scène
+     * TODO élèves : reproduire à l'identique assets/level/00-preview-example/sample1.jpg
+     * TODO élèves : plus tard, continuez le décor vers la droite en vous servant des assets mis à votre disposition
+     */
+
+
+        //TODO élève faire une animation du même genre que filter mais pour bgAnimationA
+
+        //gestion du parallaxe
+        /**
+         * Vitesse de déplacement du décor
+         * @type {number}
+         */
+        this.speed=0;
+        //initialise ce qui se passe avec le clavier
+        this.initKeyboard();
+
+    }
+
+    /**
+     * Définit ce qui se passe quand on appuie ou relache une touche du clavier
+     * ALGO : ceci est une fonction ou méthode
+     */
+    initKeyboard(){
+        let me=this;
+        this.input.keyboard.on('keydown', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                    ;
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                    ;
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.T:
+                    ;
+                    break;
+            }
+        });
+        this.input.keyboard.on('keyup', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                    ;
+                    break;
+            }
+        });
+    }
+
+    /**
+     * Cette fonction s'exécute en boucle (à peu près 60 fois par secondes)
+     */
+    update(){
+        //déplacement de la caméra
+        this.cameras.main.scrollX+=this.speed; // on aurait pu écrire : this.cameras.main.scrollX= this.cameras.main.scrollX + this.speed;
+
+        //petit effet de vibrance sur le filtre film au tout premier plan
+        this.filterFilm.setAlpha(Phaser.Math.Between(95,100)/100)
+    }
+
+
+}
